25311be8764dc80c50d228b30741c9a60d7e89e5
zhao jiankai <jk.zhaocoder@gmail.com>
2017-08-04 14:30:49 +0800

    封装Request和Response对象

--- app02.py
+++ app02.py
@@ -2,7 +2,7 @@
 
 # WSGI demo
 '''
-封装request对象和response对象
+封装request对象和response对象，以面向对象的方式，封装是为了给开发者方便
 '''
 from urllib.parse import parse_qs
 from html import escape
@@ -21,21 +21,48 @@ class Request:
             if k not in server_env.keys():
                 self.headers[k.lower()] = v
 
+class Response:
+    STATUS = {
+        200: 'OK',
+        301: 'Moved Permanently',
+        302: 'Move temporarily',
+        404: 'Not Found',
+        500: 'Internal Server Error',
+        503: 'Service Unavailable'
+    }
+
+    def __init__(self, body=None):
+        self.body = body
+        self.status = '200 OK'
+        self.headers = {
+            'content-type': 'text/html',
+            'content-length': str(len(self.body))
+        }
+
+    def set_body(self, body):
+        self.body = body
+        self.headers['content-length'] = str(len(self.body))
+
+    def set_status(self, status_code, status_text=''):
+        self.status = '{} {}'.format(status_code, self.STATUS.get(status_code, status_text))
+
+    def set_header(self, name, value):
+        self.headers[name] = value
+
+    def __call__(self, start_response): # wsgi的响应是分两部分的，一部分是头，一部分是body。
+        start_response(self.status, [(k, v) for k, v in self.headers.items()]) # 返回头
+        return [self.body.encode()] # 返回body
 
 
-def application(environ, start_response):
+def application(environ, start_response): # 封装成对象之后，应用开发者只需要写这个函数机会可以了
     request = Request(environ)
     name = request.params.get('name', ['anonymous'])[0]
     body = 'hello {}'.format(escape(name))
 
-    status = '200 OK'
-    headers = [
-        ('content-type', 'text/plain'),
-        ('content-length', str(len(body)))
-    ]
-    start_response(status, headers)
+    # start_response(status, headers)
+    # return [body.encode()]
 
-    return [body.encode()]
+    return Response(body)(start_response) # 返回给wsgi容器
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
